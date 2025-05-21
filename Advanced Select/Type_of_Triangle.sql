86ad75c2e880a0077667e3d5bcf992e08a5f300f
Jubayer Ahmed Sojib <151145553+Clear20-22@users.noreply.github.com>
2025-05-21 17:52:36 +0600

    Update Type_of_Triangle.sql

--- Advanced Select/Type_of_Triangle.sql
+++ Advanced Select/Type_of_Triangle.sql
@@ -1,1 +1,13 @@
 
+-- Write a query identifying the type of each record in the TRIANGLES table
+-- using its three side lengths A, B, and C.
+-- Output one of the following: Equilateral, Isosceles, Scalene, or Not A Triangle.
+
+SELECT
+  CASE
+    WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+    WHEN A = B AND B = C THEN 'Equilateral'
+    WHEN A = B OR A = C OR B = C THEN 'Isosceles'
+    ELSE 'Scalene'
+  END AS TriangleType
+FROM TRIANGLES;
